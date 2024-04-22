7ea9684cbd896ae9dd11f119c3a36ad54d604c92
niccolofanton <niccolofanton1997@gmail.com>
2024-04-22 15:42:13 +0200

    Add @next/third-parties package and Google Analytics integration

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,10 +1,13 @@
 import { Html, Head, Main, NextScript } from "next/document";
+import { GoogleAnalytics } from '@next/third-parties/google'
 
 
 export default function Document() {
   return (
     <Html lang="en" className={` h-auto antialiased`} suppressHydrationWarning>
-      <Head />
+      <Head>
+        <GoogleAnalytics gaId="G-KHV4Q1BQHW" />
+      </Head>
       <body className="h-full bg-zinc-200 dark:bg-[#121212]">
         <Main />
         <NextScript />
